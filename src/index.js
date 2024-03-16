50b0a94b589eede18c8e5d9134f4b5cebfcc0335
Brian <bw49078@gmail.com>
2024-03-16 16:37:49 -0500

    initial commit, bot will say pong when user says ping

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' })
+
+import { Client, IntentsBitField } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.MessageContent
+    ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', (c) => {
+    console.log(`Logged in as ${c.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+});
