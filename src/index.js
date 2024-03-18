d9027ebcb92be0a587afbeadad7155570a2ad233
Brian <bw49078@gmail.com>
2024-03-18 16:09:22 -0500

    bot can now create a message with a board inside of an embeded message

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' })
 
-import { Client, IntentsBitField } from 'discord.js';
+import { Client, IntentsBitField, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+
+let newBoard = [];
+
+const createBoard = () => {
+    for (let i = 0; i < 6; i++) {
+        newBoard.push([]);
+        for (let j = 0; j < 7; j++) {
+            newBoard[i].push(':black_circle:');
+        }
+    }
+}
+
+const printBoard = () => {
+    let board = '';
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 7; j++) {
+            board += newBoard[i][j] + ' ';
+        }
+        board += '\n';
+    }
+    return board;
+}
 
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.MessageContent
+        IntentsBitField.Flags.MessageContent,
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildMessageReactions
     ],
 });
 
@@ -23,5 +48,16 @@ client.on('messageCreate', async (message) => {
 
     if (message.content === 'ping') {
         message.reply('pong');
+    }
+
+    if (message.content === '!play c4') {
+        createBoard();
+        let board = printBoard();
+        const embed = new EmbedBuilder()
+            .setTitle('Connect 4')
+            .setDescription('Type !join to join the game')
+            .addFields({name: 'Board', value: board});
+
+        message.channel.send({embeds: [embed]});
     }
 });
