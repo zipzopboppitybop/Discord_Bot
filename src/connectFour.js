4607d163f4112ebc625d067c4e229d659172a08f
Brian <bw49078@gmail.com>
2024-03-19 22:02:40 -0500

    test commit

--- src/connectFour.js
+++ src/connectFour.js
@@ -20,69 +20,50 @@ export default class ConnectFour {
         this.printedBoard = '';
         for (let i = 0; i < 6; i++) {
             for (let j = 0; j < 7; j++) {
-                this.printedBoard += this.board[i][j] + ' ';
+                this.printedBoard += this.board[i][j] + ' ';                    
             }
             this.printedBoard += '\n';
         }
+        
         return this.printedBoard;
     }
 
     dropPiece = (column) => {
         if (this.player1) {
+
+            this.playersTurn = "Yellow's Turn";
+            this.player1 = 0;
+
             if (this.board[5][column] === ':black_circle:') {
                 this.board[5][column] = ':red_circle:';
-                this.player1 = 0;
-                this.playersTurn = "Yellow's Turn";
             } else if (this.board[4][column] === ':black_circle:') {
                 this.board[4][column] = ':red_circle:';
-                this.player1 = 0;
-                this.playersTurn = "Yellow's Turn";
             } else if (this.board[3][column] === ':black_circle:') {
                 this.board[3][column] = ':red_circle:';
-                this.player1 = 0;
-                this.playersTurn = "Yellow's Turn";
             } else if (this.board[2][column] === ':black_circle:') {
                 this.board[2][column] = ':red_circle:';
-                this.player1 = 0;
-                this.playersTurn = "Yellow's Turn";
-            }
-            else if (this.board[1][column] === ':black_circle:') {
+            } else if (this.board[1][column] === ':black_circle:') {
                 this.board[1][column] = ':red_circle:';
-                this.player1 = 0;
-                this.playersTurn = "Yellow's Turn";
-            }
-            else if (this.board[0][column] === ':black_circle:') {
+            } else if (this.board[0][column] === ':black_circle:') {
                 this.board[0][column] = ':red_circle:';
-                this.player1 = 0;
-                this.playersTurn = "Yellow's Turn";
             }
         } else {
+
+            this.playersTurn = "Red's Turn";
+            this.player1 = 1;
+
             if (this.board[5][column] === ':black_circle:') {
                 this.board[5][column] = ':yellow_circle:';
-                this.player1 = 1;
-                this.playersTurn = "Red's Turn";
             } else if (this.board[4][column] === ':black_circle:') {
                 this.board[4][column] = ':yellow_circle:';
-                this.player1 = 1;
-                this.playersTurn = "Red's Turn";
             } else if (this.board[3][column] === ':black_circle:') {
                 this.board[3][column] = ':yellow_circle:';
-                this.player1 = 1;
-                this.playersTurn = "Red's Turn";
             } else if (this.board[2][column] === ':black_circle:') {
                 this.board[2][column] = ':yellow_circle:';
-                this.player1 = 1;
-                this.playersTurn = "Red's Turn";
-            }
-            else if (this.board[1][column] === ':black_circle:') {
+            } else if (this.board[1][column] === ':black_circle:') {
                 this.board[1][column] = ':yellow_circle:';
-                this.player1 = 1;
-                this.playersTurn = "Red's Turn";
-            }
-            else if (this.board[0][column] === ':black_circle:') {
+            } else if (this.board[0][column] === ':black_circle:') {
                 this.board[0][column] = ':yellow_circle:';
-                this.player1 = 1;
-                this.playersTurn = "Red's Turn";
             }
         }
 
